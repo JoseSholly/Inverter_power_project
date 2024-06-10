f50bdbdfb8156d4df23fe85ed8cd77d481179e89
JoseSholly <josepholawuyi30@gmail.com>
2024-06-10 12:35:08 +0100

    Add superuser

--- inverter_project/power_calculator/models.py
+++ inverter_project/power_calculator/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Appliance(models.Model):
+    name= models.CharField(max_length=100, null=False)
+    quantity= models.PositiveIntegerField(default=1, null=False, help_text="Numbers of appliance")
+    power_rating= models.PositiveIntegerField(null=False, help_text= "Power rating in watts")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated']
+        indexes = [
+            models.Index(fields=['-updated']),]
+
+    def __str__(self) -> str:
+        return self.name
