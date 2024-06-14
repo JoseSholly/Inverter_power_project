d6cb05df9b024de911ddfa7a2af90256dd5fd1fe
JoseSholly <josepholawuyi30@gmail.com>
2024-06-14 11:18:59 +0100

    Add: calculation delete view

--- inverter_project/power_calculator/urls.py
+++ inverter_project/power_calculator/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import CalculationCreateView, CalculationsListView, CalculationUpdateView
+from .views import CalculationCreateView, CalculationsListView, CalculationUpdateView, CalculationDeleteView
 
 urlpatterns = [
     path('calculate/', CalculationCreateView.as_view(), name='calculation-create'),
     path('calculations-list/', CalculationsListView.as_view(), name='calculation-list'),
     path('calculation/<uuid:pk>/', CalculationUpdateView.as_view(), name='calculation-update'),
+    path('calculation/<uuid:pk>/delete/', CalculationDeleteView.as_view(), name='calculation-delete'),
 ]
--- inverter_project/power_calculator/views.py
+++ inverter_project/power_calculator/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from .models import Calculation
 from .serializers import CalculationSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 
 class CalculationCreateView(generics.CreateAPIView):
     queryset = Calculation.objects.all()
@@ -14,3 +16,16 @@ class CalculationsListView(generics.ListAPIView):
 class CalculationUpdateView(generics.UpdateAPIView):
     queryset = Calculation.objects.all()
     serializer_class = CalculationSerializer
+
+class CalculationDeleteView(generics.DestroyAPIView):
+    queryset = Calculation.objects.all()
+    serializer_class = CalculationSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        
+        instance = self.get_object()
+        
+        calculation_id = instance.id
+        self.perform_destroy(instance)
+        return Response({"message": f"Calculation {calculation_id} deleted successfully"})
+
