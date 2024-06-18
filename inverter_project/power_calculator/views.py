9556195b3b11afa85ae7cfef096d7d34142bceee
JoseSholly <josepholawuyi30@gmail.com>
2024-06-18 14:38:54 +0100

    Add staff permission

--- inverter_project/power_calculator/views.py
+++ inverter_project/power_calculator/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Calculation
 from .serializers import CalculationSerializer
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from .permissions import IsStaffUser
 
 class CalculationCreateView(generics.CreateAPIView):
     queryset = Calculation.objects.all()
@@ -11,17 +11,17 @@ class CalculationCreateView(generics.CreateAPIView):
 class CalculationsListView(generics.ListAPIView):
     queryset= Calculation.objects.all()
     serializer_class= CalculationSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsStaffUser]
 
 class CalculationUpdateView(generics.UpdateAPIView):
     queryset = Calculation.objects.all()
     serializer_class = CalculationSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsStaffUser]
 
 class CalculationDeleteView(generics.DestroyAPIView):
     queryset = Calculation.objects.all()
     serializer_class = CalculationSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsStaffUser]
 
     def destroy(self, request, *args, **kwargs):
         
