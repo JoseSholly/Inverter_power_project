2d3a36b268f5a316a75b761e56e2ef3076530eb2
JoseSholly <josepholawuyi30@gmail.com>
2024-06-18 14:23:23 +0100

    Refactoring code

--- inverter_project/inverter_project/urls.py
+++ inverter_project/inverter_project/urls.py
@@ -28,7 +28,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="Home Inverter Calculator API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
@@ -42,10 +42,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("api-schema/", get_restframe_work_schema(title="API Schema", description="Guide for the REST API"), name="api_schema"),
    path('api/docs/swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('api/redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls, name="admin"),
-    # path('api/', include('api.urls')), # localhost:8000/api
     path('api/v1/power_calculator/', include("power_calculator.urls",))
 
 ]
--- inverter_project/power_calculator/views.py
+++ inverter_project/power_calculator/views.py
@@ -2,24 +2,26 @@ from rest_framework import generics
 from .models import Calculation
 from .serializers import CalculationSerializer
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAdminUser
 
 class CalculationCreateView(generics.CreateAPIView):
     queryset = Calculation.objects.all()
     serializer_class = CalculationSerializer
 
-
 class CalculationsListView(generics.ListAPIView):
     queryset= Calculation.objects.all()
     serializer_class= CalculationSerializer
+    permission_classes = [IsAdminUser]
 
 class CalculationUpdateView(generics.UpdateAPIView):
     queryset = Calculation.objects.all()
     serializer_class = CalculationSerializer
+    permission_classes = [IsAdminUser]
 
 class CalculationDeleteView(generics.DestroyAPIView):
     queryset = Calculation.objects.all()
     serializer_class = CalculationSerializer
+    permission_classes = [IsAdminUser]
 
     def destroy(self, request, *args, **kwargs):
         
