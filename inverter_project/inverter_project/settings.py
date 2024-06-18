bc820e9681b71ba6f429fbd28b22eaf725d93176
JoseSholly <josepholawuyi30@gmail.com>
2024-06-18 15:21:42 +0100

    Local: Add static files

--- inverter_project/inverter_project/settings.py
+++ inverter_project/inverter_project/settings.py
@@ -28,11 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"
-# DEBUG= True
+# DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+DEBUG= True
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
-# ALLOWED_HOSTS= []
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS= []
 
 
 # Application definition
@@ -135,11 +135,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
