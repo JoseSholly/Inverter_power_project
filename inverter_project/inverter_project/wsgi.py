5f65c6a83b0c4f74f68bca04dba1c4a8728afed0
JoseSholly <josepholawuyi30@gmail.com>
2024-06-18 15:53:08 +0100

    Live: Testing static files

--- inverter_project/inverter_project/wsgi.py
+++ inverter_project/inverter_project/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
+from .settings import BASE_DIR
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inverter_project.settings')
 
 application = get_wsgi_application()
+# application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
