5dfab80fa02e3a9ea7bb0cbb406808167da0fdd5
JoseSholly <josepholawuyi30@gmail.com>
2024-11-05 17:53:31 +0100

    Urls test %

--- inverter_project/power_calculator/tests/test_urls.py
+++ inverter_project/power_calculator/tests/test_urls.py
@@ -1,7 +1,7 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from api.views import AppliancesListView
-
+from api.views import AppliancesListView, CalculationsListView, CalculationCreateView, CalculationUpdateView, CalculationDeleteView
+import uuid
 class TestUrls(SimpleTestCase):
     def test_appliances_list(self):
         url = reverse("appliance-list")
@@ -9,3 +9,44 @@ class TestUrls(SimpleTestCase):
 
         # Check if the resolved function corresponds to AppliancesListView's as_view function
         self.assertEqual(resolved_func.view_class, AppliancesListView)
+
+    def test_calculations_list(self):
+        url = reverse("calculation-list")
+        resolved_func = resolve(url).func
+
+        # Check if the resolved function corresponds to CalculationsListView's as_view function
+        self.assertEqual(resolved_func.view_class, CalculationsListView)
+
+    def test_calculation_create(self):
+        url: str = reverse('calculation-create')
+        resolved_url = resolve(url).func
+
+        # Check if the resolved function corresponds to CalculationCreateView's as_view function
+        self.assertAlmostEqual(resolved_url.view_class, CalculationCreateView) 
+
+    
+    def test_calculation_update(self):
+        # Create a sample UUID for testing
+        sample_uuid = uuid.uuid4()
+
+        # Reverse the URL to generate the correct path with the UUID
+        url = reverse("calculation-update", kwargs={"pk": sample_uuid})
+
+        # Resolve the URL and check if it maps to the correct view
+        resolved_url = resolve(url).func
+
+        # Check if the resolved function corresponds to CalculationCreateView's as_view function
+        self.assertAlmostEqual(resolved_url.view_class, CalculationUpdateView) 
+
+    def test_calculation_delete(self):
+        # Create a sample UUID for testing
+        sample_uuid = uuid.uuid4()
+
+        # Reverse the URL to generate the correct path with the UUID
+        url = reverse("calculation-delete", kwargs={"pk": sample_uuid})
+
+        # Resolve the URL and check if it maps to the correct view
+        resolved_url = resolve(url).func
+
+        # Check if the resolved function corresponds to CalculationCreateView's as_view function
+        self.assertAlmostEqual(resolved_url.view_class, CalculationDeleteView) 
