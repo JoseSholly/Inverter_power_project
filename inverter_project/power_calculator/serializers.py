908a16fef50e24dfb3bc50bc18b57cc834e803cd
JoseSholly <josepholawuyi30@gmail.com>
2024-11-09 13:55:47 +0100

    Refactored calculation

--- inverter_project/power_calculator/serializers.py
+++ inverter_project/power_calculator/serializers.py
@@ -80,10 +80,4 @@ class CalculationSerializer(serializers.ModelSerializer):
         return instance
         
     def _recalculate(self, calculation):
-        calculation.calculate_total_load()
-        calculation.calculate_total_inverter_rating()
-        calculation.calculate_total_battery_capacity()
-        calculation.calculate_no_of_battery()
-        calculation.calculate_solar_panel_capacity_needed()
-        calculation.calculate_no_of_panel()
-        calculation.calculate_total_current()
+        calculation.perform_calculations()
